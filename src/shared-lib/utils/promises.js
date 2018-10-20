68ffdba4adf65add487c892f9efe7e68589d4fd3
Henrique Rangel <hrangel@nucleo235.com.br>
2018-10-20 11:46:59 -0300

    Adicicionada shared-lib ao gitignore

--- src/shared-lib/utils/promises.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.promiseSerial = funcs => funcs.reduce((promise, func) => promise.then(result => func().then(Array.prototype.concat.bind(result))), Promise.resolve([]));
-//# sourceMappingURL=promises.js.map
